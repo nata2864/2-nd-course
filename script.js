c23abf9c9cfcdba1ea61808669d4d3db4ca8e001
Natalia Bashirova <tac-2014@yandex.ru>
2024-09-27 22:20:36 +0200

    Добавляет логику игры №2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/Функция нахождения рандомного числа/
 const getRandomInteger = (a, b) => {
     const lower = Math.ceil(Math.min(a, b));
     const upper = Math.floor(Math.max(a, b));
@@ -6,12 +8,19 @@ const getRandomInteger = (a, b) => {
   };
 
 
-
+/Код к игре №1/
 const guessNumber = (randomInteger) => {
 
-    let answer = prompt('Угадайте число');
+    let answer = prompt('Угадайте число (или нажмите Отмена для выхода)');
+
+   
 
     while (answer != randomInteger) {
+
+      if (answer === null) {
+        alert('Игра окончена. Вы вышли.');
+        return;
+      }
   
       answer < randomInteger ? alert('Загаданное число больше') : alert('Загаданное число меньше');
   
@@ -22,5 +31,44 @@ const guessNumber = (randomInteger) => {
 
 };
 
+/Код к игре №2/
 
+const getUserAnswer = ( randomInteger1, randomInteger2, randomIndex) => {
+
+let userAnswer; 
+let operationResult; 
+ 
+    switch(randomIndex) {
+        case 1: 
+        userAnswer = prompt(`Сколько будет ${randomInteger1} + ${randomInteger2}? Напишите ответ`);
+        operationResult = randomInteger1 + randomInteger2;
+          break;
+      
+          case 2: 
+          userAnswer = prompt(`Сколько будет ${randomInteger1} - ${randomInteger2}? Напишите ответ`);
+          operationResult = randomInteger1 - randomInteger2;
+            break;
+    
+            case 3: 
+            userAnswer = prompt(`Сколько будет ${randomInteger1} * ${randomInteger2}? Напишите ответ`);
+        operationResult = randomInteger1 * randomInteger2;
+          break;
+    
+          case 4: 
+          userAnswer = prompt(`Сколько будет ${randomInteger1} / ${randomInteger2}? Напишите ответ`);
+        operationResult = randomInteger1 / randomInteger2;
+          break;
+      }
+
+
+if (userAnswer==operationResult){
+
+  alert('Молодец! Ответил правильно');
+
+}else {
+
+  alert(`Неверно. Правильный ответ: ${operationResult}`);
+   
+}
+}
   
