a463edba8d378f381d296b4f1104d5a19945c7cb
Natalia Bashirova <tac-2014@yandex.ru>
2024-10-20 15:51:57 +0200

    Добавляет логику игры №6

--- script.js
+++ script.js
@@ -7,24 +7,52 @@ const getRandomInteger = (a, b) => {
     return Math.floor(result);
   };
 
+// Функция нажатия кнопки "Отмена"
+const checkForCancel = (input) => {
+  if (input === null) {
+    alert('Игра окончена. Вы вышли.');
+    return true;  
+  }
+  return false;  
+};
+
+//Функция проверки на ввод числа
+const checkNumber = (input) => {
+
+  const parsedInput = Number(input); // Преобразуем строку в число
+
+  if (Number.isNaN(parsedInput) || input.trim() === '') {
+    alert('Ошибка: Пожалуйста, введите корректное число.');
+
+    return true;
+  }
+
+    return false; 
+};
+
+
 
 //Код к игре №1
-const guessNumber = (randomInteger) => {
+const guessNumber = () => {
 
-    let answer = prompt('Угадайте число (или нажмите Отмена для выхода)');
+  const randomInteger = getRandomInteger(1,100)
 
-   
+    let userAnswer = prompt('Угадайте число');
+    
+    if (checkForCancel(userAnswer) || checkNumber(userAnswer)) return;
 
-    while (answer != randomInteger) {
+    let answer = Number(userAnswer);
 
-      if (answer === null) {
-        alert('Игра окончена. Вы вышли.');
-        return;
-      }
-  
+    
+    while (+answer !== randomInteger) {
+
       answer < randomInteger ? alert('Загаданное число больше') : alert('Загаданное число меньше');
   
-      answer = prompt('Угадайте число');
+      userAnswer = prompt('Угадайте число');
+
+      if (checkForCancel(userAnswer) || checkNumber(userAnswer)) return;
+
+     let answer = Number(userAnswer);
   }
 
   alert(`Молодец. Загаданное число было ${randomInteger}`)
@@ -32,151 +60,133 @@ const guessNumber = (randomInteger) => {
 };
 
 //Код к игре №2
-
 const operations = ['+', '-', '*', '/'];
 
 const calculate = (a, operation, b) => {
+
   const func = new Function('a', 'b', `return a ${operation} b;`);
+
   return func(a, b);
 };
 
-const getUserAnswer = ( randomInteger1, randomInteger2, randomIndex) => {
+const getUserAnswer = () => {
+
+  const randomInteger1 = getRandomInteger(1,100);
+  const randomInteger2 = getRandomInteger(1,100);
+  const randomIndex = getRandomInteger(0,3);
 
 const userAnswer = prompt(`Сколько будет ${randomInteger1}${operations[randomIndex]}${randomInteger2}? Напишите ответ`);
+
+ if (checkForCancel(userAnswer) || checkNumber(userAnswer)) return;
+
 const operationResult = calculate(randomInteger1, operations[randomIndex], randomInteger2);
 
++userAnswer===operationResult? alert('Молодец! Ответил правильно'): alert(`Неверно. Правильный ответ: ${operationResult}`)
 
-if (+userAnswer===operationResult){
-
-  alert('Молодец! Ответил правильно');
-
-}else {
-
-  alert(`Неверно. Правильный ответ: ${operationResult}`);
-   
-}
 }
   
 //Код к игре №3
-
-userText = '';
 const reverUserText = () => {
   
   let userText = prompt ('Введите текст');
+
+  if (checkForCancel(userText)) return;
   
-  let reverseText = userText.split('').reverse().join('');
-  
-  alert(reverseText);
+  alert(userText.split('').reverse().join(''));
+ 
 };
-
-
 
 //Код к игре №4
 const quiz = [
   {
       question: "Какой цвет неба?",
-      options: ["1. Красный", "2. Синий", "3. Зеленый"],
-      correctAnswer: 2 // номер правильного ответа
+      options: [" 1. Красный ", " 2. Синий ", " 3. Зеленый "],
+      correctAnswer: 2 
   },
   {
       question: "Сколько дней в неделе?",
-      options: ["1. Шесть", "2. Семь", "3. Восемь"],
+      options: [" 1. Шесть ", " 2. Семь ", " 3. Восемь "],
       correctAnswer: 2
   },
   {
       question: "Сколько у человека пальцев на одной руке?",
-      options: ["1. Четыре", "2. Пять", "3. Шесть"],
+      options: [" 1. Четыре ", " 2. Пять ", " 3. Шесть "],
       correctAnswer: 2
   }
 ];
 
-let correctAnswer = 0;
+let totaalCorrectAnswers = 0;
 
 let createQuizQuestions = () => {
   
   for (let index = 0; index < quiz.length; index++) {
 
-    let userAnswer = prompt(`Ответьте на вопрос: " ${quiz[index].question}"
-    Варианты ответов: ${quiz[index].options}
-    (Для выхода : нажмите "Отмена")`);
+    const {question, options, correctAnswer} = quiz[index]
+
+    let userAnswer = prompt(` ${question}\n ${options.join('')} `);
     
-        if (userAnswer === null) {
-          alert('Вы отказались от ввода');
-          break;
-        } 
+    if (checkForCancel(userAnswer) || checkNumber(userAnswer)) return;
     
-          if (+userAnswer === quiz[index].correctAnswer){
-          correctAnswer++
-          
+          if (+userAnswer === correctAnswer){
+            totaalCorrectAnswers++  
         } 
     }
     
-    alert (`Количество верных ответов: ${correctAnswer}`);
+    alert (`Количество верных ответов: ${totaalCorrectAnswers}`);
 }
 
 
 //Код к игре №5
+const getMessageResultGame = (playerChoice1 , playerChoice2) => {
 
+  if (playerChoice1 === playerChoice2) {
+    return 'Ничья!';
+  }
+
+  const winUsersAnswers = {
+    'КАМЕНЬ': 'НОЖНИЦЫ',
+    'НОЖНИЦЫ': 'БУМАГА',
+    'БУМАГА': 'КАМЕНЬ',
+  };
+
+ return winUsersAnswers [playerChoice1] === playerChoice2 ? 'Вы выиграли!' : 'Вы проиграли!';
+}
+  
 
 
 const createContantGame = () => {
 
-  let userGameChoice = prompt('Что выберите: КАМЕНЬ, НОЖНИЦЫ или БУМАГА?');
-  userGameChoice = userGameChoice.trim().toUpperCase();
-  
+  let userGameChoice = prompt('Что выберите: КАМЕНЬ, НОЖНИЦЫ или БУМАГА?').trim().toUpperCase();
+ 
   const gamesVariants = ['КАМЕНЬ', 'НОЖНИЦЫ', 'БУМАГА'];
   
   const getRandomIndex = (length) => Math.floor(Math.random() * length);
   
-  let computerGameChoice = gamesVariants[getRandomIndex(3)];
-  
-  let message;
-  
-  const getMessageResultGame = (playerChoice1, playerChoice2) => {
-   
-      let winMessage = 'Вы выиграли!';
-      let lostMessage = 'Вы проиграли!'; 
-      let drawMessage = 'Ничья!';
-  
-      if (playerChoice1 === playerChoice2) {
-          return drawMessage;
-      }
-   
-      switch (playerChoice1) {
-          case 'КАМЕНЬ':
-              if (playerChoice2 === 'НОЖНИЦЫ') {
-                  message = winMessage;
-              } else {
-                  message = lostMessage;
-              }             
-              break;
-  
-          case 'НОЖНИЦЫ':
-              if (playerChoice2 === 'БУМАГА') {
-                  message = winMessage;
-              } else {
-                  message = lostMessage;
-              }   
-              break;
-  
-          case 'БУМАГА':
-              if (playerChoice2 === 'КАМЕНЬ') {
-                  message = winMessage;
-              } else {
-                  message = lostMessage;
-              }   
-              break;
-  
-          default:
-              message = "Некорректный выбор. Пожалуйста, выберите КАМЕНЬ, НОЖНИЦЫ или БУМАГА.";
-              break;
-      }
-  
-      return message;
-  }
-  
-  alert( `Вы выбрали : ${userGameChoice}. Компьютер выбрал : ${computerGameChoice}`)
-  alert(getMessageResultGame(userGameChoice, computerGameChoice))
+  let computerGameChoice = gamesVariants[getRandomIndex(gamesVariants.length)];
 
+    alert( `Вы выбрали : ${userGameChoice}. Компьютер выбрал : ${computerGameChoice}`);
+
+    alert(getMessageResultGame (userGameChoice, computerGameChoice))
+
+} 
+
+//Код к игре №6
+const getRandomColor = () => {
+
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  
+  for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const changeBackgroundColor = () => {
+
+const backgroundGame = document.querySelector ('#gameblock');
+
+backgroundGame.style.backgroundColor = getRandomColor();
 
 }
+
